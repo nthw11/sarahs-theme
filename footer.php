7917d20e3b5926f0f04653f7e5aed53daa661845
Nathaniel <nthw.dev@gmail.com>
2021-11-15 16:33:31 -0500

    site is up
    done for the day, ready to test on test server

--- footer.php
+++ footer.php
@@ -1,4 +1,22 @@
 <footer class="site-footer">
+  <footer>
+    <div id="social-links-div">
+      <a href="https://www.instagram.com/Sarah.Wallace.writer/" target="blank class=" social-link">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="https://www.goodreads.com/author/show/7307580.Sarah_Wallace" target="blank" class="social-link">
+        <i class="fab fa-goodreads"></i>
+      </a>
+      <a href="https://pin.it/1pqtRyO" target="blank" class="social-link">
+
+        <i class="fab fa-pinterest-square"></i>
+      </a>
+      <i class="fas fa-feather"></i>
+      <a href="<?php echo site_url('/order') ?>" class="social-link">
+        <i class="fas fa-book"></i>
+      </a>
+    </div><!-- #social-links-div -->
+  </footer>
   </div><!-- .main-page-body-->
 </footer>
 <?php wp_footer(); ?>
--- functions.php
+++ functions.php
@@ -4,7 +4,8 @@ function sarah_files()
 {
 
   wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css2?family=Alice&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  // wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css');
 
   wp_enqueue_script('main-js', get_theme_file_uri('/scripts.js'), array('jquery'), '', true);
   wp_enqueue_style('sarah_main_styles', get_stylesheet_uri());
